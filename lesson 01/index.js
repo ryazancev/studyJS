9a429977a8e6e66767c9dcb346cecc6229734578
Evgeniy Ryazancev <ryazancev.e@gmail.com>
2020-11-20 23:46:41 +0300

    Первый урок

--- lesson 01/index.js
+++ lesson 01/index.js
@@ -1,19 +1,9 @@
-let money = 30000; 
-let income = 'кухня'; 
-let addExpenses = 'арнеда, интернет, такси, автобус, еда'; 
-let deposit = true; 
-let mission = 100000; 
-let period = 6;
+let money; 
+let income; 
+let addExpenses; 
+let deposit; 
+let mission; 
+let perio;
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-
-console.log('Период равен ' + period + ' месяцев');
-console.log('Цель заработать ' + mission + ' рублей');
-
-addExpenses = addExpenses.toLowerCase().split(', ');
-console.log(addExpenses);
-
-let budgetDay = money / 30;
-console.log(budgetDay);
+alert('JavaScript awesome!');
+console.log('JavaScript интерпретируемый язык!');
