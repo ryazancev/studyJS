fcbaa567e5d26cce4a817cdb28a2153131b4160f
Evgeniy Ryazancev <ryazancev.e@gmail.com>
2020-11-20 23:17:34 +0300

    Второе задание

--- lesson 01/index.js
+++ lesson 01/index.js
@@ -1,9 +1,19 @@
-let money; 
-let income; 
-let addExpenses; 
-let deposit; 
-let mission; 
-let perio;
+let money = 30000; 
+let income = 'кухня'; 
+let addExpenses = 'арнеда, интернет, такси, автобус, еда'; 
+let deposit = true; 
+let mission = 100000; 
+let period = 6;
 
-alert('JavaScript awesome!');
-console.log('JavaScript интерпретируемый язык!');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+
+addExpenses = addExpenses.toLowerCase().split(', ');
+console.log(addExpenses);
+
+let budgetDay = money / 30;
+console.log(budgetDay);
