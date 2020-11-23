7eafab055ca18b8093e81aa9bb8444b16644d422
Evgeniy Ryazancev <ryazancev.e@gmail.com>
2020-11-23 15:44:15 +0300

    Третье доп задание

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
 let lang;
-lang = 'en';
+lang = 'ru';
 
-// if (lang === 'ru') {
-//     console.log('пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс');
-// } else if (lang === 'en') {
-//     console.log('mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn');
-// }
+if (lang === 'ru') {
+    console.log('пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс');
+} else if (lang === 'en') {
+    console.log('mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn');
+}
 
-// switch (lang) {
-//     case 'ru':
-//         console.log('пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс');
-//         break;
-//     case 'en':
-//         console.log('mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn');
-//         break;
-// }
-
-// let arr = [
-//     ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'], 
-//     ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn']
-// ]
+switch (lang) {
+    case 'ru':
+        console.log('пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс');
+        break;
+    case 'en':
+        console.log('mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn');
+        break;
+}
 
 
 let arr = {
@@ -27,3 +22,12 @@ let arr = {
 	'en':['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'],
 };
 console.log(arr[lang]);
+
+// Вторая часть задания
+
+let namePerson = ''; 
+let res = namePerson === 'Артем' 
+    ? console.log('Директор') 
+    : namePerson === 'Максим' 
+    ? console.log('Преподаватель') 
+    : console.log('Студент');
