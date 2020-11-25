028a5df9fdf66747e078ab1f79f1db111105c033
Evgeniy Ryazancev <ryazancev.e@gmail.com>
2020-11-25 13:18:13 +0300

    Отредактировал код

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function changeString(str) {
     if (typeof str !== 'string') {
         alert('В качестве аргумента необходимо передать строку');
-    } else {
-        console.log(str.trim());
     }
 
-    if (str.length > 30) {
-        console.log(str.substr(0, 30) + '...');
+    if (str.length >= 30) {
+        console.log(str.trim().substr(0, 30) + '...');
+    } else {
+        console.log(str.trim());
     }
 }
 
