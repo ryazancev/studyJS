a532b4244ff29fddde9d3f5b2da2d2f8c8ee8b5e
Evgeniy Ryazancev <ryazancev.e@gmail.com>
2020-11-28 17:35:40 +0300

    Переделал седьмое усложненное

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
 // Создать массив week и записать в него дни недели в виде строк
-let week = ['вск', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
-
-// ·        Вывести на экран все дни недели
-document.write(week);
-document.write('<br>');
-
-// ·        Каждый из них с новой строчки
-week.forEach(function(item) {
-    document.write(item, '<br>');
-});
-
-// ·        Выходные дни - курсивом
-document.write('<i>', week[0], '\n', week[6], '</i>');
-document.write('<br>');
-
-// ·        Текущий день - жирным шрифтом(использовать объект даты)
-let date = new Date(),
+let week = ['вск', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'],
+    date = new Date(),
     weekday = date.getDay();
-    
-    if (weekday === 0 || weekday === 6) {
-        document.write('<b>', '<i>', week[weekday], '</i>', '</b>');
-        document.write('<br>');
+
+for (let i = 0; i < week.length; i++) {
+    if (i === weekday) {
+        if (week[i] === 'сб' || week[i] === 'вск') {
+            document.write('<b>', '<i>', week[i], '</i>', '</b>');
+        }
+    } else if (week[i] === 'вск' || week[i] === 'сб' ) {
+        document.write('<i>', week[i], '</i>', '<br>');
     } else {
-        document.write('<b>', week[weekday], '</b>');
-        document.write('<br>');
+        document.write(week[i], '<br>');
     }
+}
 
 
 
