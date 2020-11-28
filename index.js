99e54722a392065d639e1afa7e4d2699c07e9266
Evgeniy Ryazancev <ryazancev.e@gmail.com>
2020-11-28 16:54:05 +0300

    Шестое сложное

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
 'use strict';
 
+// Кол-во попыток пользователя должно быть ограничено: 10
 
-function guessNumber() {
+
+// — если пользователь вводит правильное число, то бот выводит "Поздравляю, Вы угадали!!! Хотели бы сыграть еще?", при нажатии ОК игра перезапускается (снова 10 попыток и новое загаданное число)
+// — если пользователь ввел не число, то выводит сообщение "Введи число!" и предлагает ввести новый вариант;
+// — если пользователь нажимает "Отмена", то игра выводит прощальное сообщение и завершается.
+// — если закончились попытки то программа сообщает: "Попытки закончились, хотите сыграть еще?"
+
+let counter = 10;
+
+function guessNumber(count) {
     let userNum = prompt('Угадай число от 1 до 100'),
         currentNum = 10;
+        
+        counter--;
     
     function isNumber(n) {
         return !isNaN(parseFloat(n)) && isFinite(n);
     }
 
     function compareNumber(userNumber) {
+        
         if (isNumber(userNum)) {
             if (userNumber > currentNum) {
-                alert('Загаданное число меньше, введите новый вариант');
+                alert('Загаданное число меньше. У вас осталось ' + counter + ' попыток', 'введите новый вариант');
                 guessNumber();
             } else if (userNumber < currentNum) {
-                alert('Загаданное число больше, введите новый вариант');
+                alert('Загаданное число больше. У вас осталось ' + counter + ' попыток', 'введите новый вариант');
                 guessNumber();
             } else if (userNumber == currentNum) {
-                return alert('Поздравляю, Вы угадали!!!');
+                alert('Поздравляю, Вы угадали!!!');
+                guessNumber(counter = 10);
             }
         } else if (userNum === null) {
             return alert('Игра окончена!');
+        } else if (counter === 0) {
+
         } else {
             alert('Введи число!');
             guessNumber();
@@ -30,6 +45,6 @@ function guessNumber() {
     return compareNumber(userNum);
 }
 
-guessNumber();
+guessNumber(counter);
 
 
