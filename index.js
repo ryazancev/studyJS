5c31437d65eebf5e45718284525b838eb3d33e2b
Evgeniy Ryazancev <ryazancev.e@gmail.com>
2020-11-28 16:05:46 +0300

    Седьмое сложное

--- index.js
+++ index.js
@@ -1,96 +1,31 @@
-'use strict';
+// Создать массив week и записать в него дни недели в виде строк
+let week = ['вск', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
 
-// Обьявляем переменные
-let money,
-    start = getStart(),
-    appData = {
-        income: {},
-        addIncome: [],
-        expenses: {},
-        addExpenses: [],
-        budget: money,
-        budgetDay: 0,
-        budgetMonth: 0,
-        expensesMonth: 0,
-        deposit: false,
-        mission: 100000,
-        period: 6,
-        asking: function() {
-            let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую',
-                'Квартплата, проездной, кредит');
-            appData.addExpenses = addExpenses.toLowerCase().split(', ');
-            appData.deposit = confirm('Есть ли у вас депозит в банке?');
+// ·        Вывести на экран все дни недели
+document.write(week);
+document.write('<br>');
 
-            for (let i = 0; i < 2; i++) {
-                let question = prompt('Введите обязательную статью расходов'),
-                    answer;
-                
-                do {
-                    answer = prompt('Во сколько это обойдется?')
-                }
-                while (!isNumber(answer))
+// ·        Каждый из них с новой строчки
+week.forEach(function(item) {
+    document.write(item, '<br>');
+});
 
-                appData.expenses[question] = +answer;
-            } 
-        },
-        getExpensesMonth: function() {
-            for (let key in appData.expenses) {
-                appData.expensesMonth += appData.expenses[key];
-            } 
-        },
-        getBudget: function() {
-            appData.budgetMonth = appData.budget - appData.expensesMonth;
-            appData.budgetDay = appData.budgetMonth / 30;
-        },
-        getTargetMonth: function() {
+// ·        Выходные дни - курсивом
+document.write('<i>', week[0], '\n', week[6], '</i>');
+document.write('<br>');
 
-            if ((appData.mission / appData.budgetDay) > 0) {
-                return ('Цель будет достигнута за: ' + Math.ceil(appData.mission / appData.budgetDay) + ' месяцев');
-            } else {
-                return ('Цель не будет достигнута');
-            }
-        },
-        getStatusIncome: function() {
-            if (appData.budgetDay > 1200) {
-                return ('У вас высокий уровень дохода');
-            } else if (appData.budgetDay >= 600 && appData.budgetDay <= 1200) {
-                return ('У вас средний уровень дохода');
-            } else if (appData.budgetDay < 600 && appData.budgetDay > 0) {
-                return ('К сожалению у вас уровень дохода ниже среднего')
-            } else {
-                return ('Что то пошло не так')
-            }
-        }
-    };
-
-
-// Функционал
-
-function isNumber(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
-}
-
-function getStart() {
-    do {
-        money = prompt('Ваш месячный доход?');
-    }
-    while (!isNumber(money));
+// ·        Текущий день - жирным шрифтом(использовать объект даты)
+let date = new Date(),
+    weekday = date.getDay();
     
-    money = +money;
-}
+    if (weekday === 0 || weekday === 6) {
+        document.write('<b>', '<i>', week[weekday], '</i>', '</b>');
+        document.write('<br>');
+    } else {
+        document.write('<b>', week[weekday], '</b>');
+        document.write('<br>');
+    }
 
 
-appData.asking();
-appData.getExpensesMonth();
-appData.getBudget();
 
-for (let key in appData) {
-    console.log('Наша программа включает в себя данные: ');
-    console.log(key + ' ' + ' ' + appData[key]);
-}
-
-// Выводы в консоль
-console.log('Расходы за месяц: ' + appData.budgetMonth);
-console.log(appData.getTargetMonth());
-console.log(appData.getStatusIncome());
 
