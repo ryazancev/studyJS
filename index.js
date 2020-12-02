304436a6c06224a3746252a045ff8fb36d8e490b
Evgeniy Ryazancev <ryazancev.e@gmail.com>
2020-12-03 01:32:45 +0300

    Девятое задание

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-// Создать массив week и записать в него дни недели в виде строк
-let week = ['вск', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'],
-    date = new Date(),
-    weekday = date.getDay();
-
-for (let i = 0; i < week.length; i++) {
-    if (i === weekday) {
-        if (week[i] === 'сб' || week[i] === 'вск') {
-            document.write('<b>', '<i>', week[i], '</i>', '</b>');
-        }
-    } else if (week[i] === 'вск' || week[i] === 'сб' ) {
-        document.write('<i>', week[i], '</i>', '<br>');
-    } else {
-        document.write(week[i], '<br>');
-    }
-}
-
 
 
 
