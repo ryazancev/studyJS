c99a0e557437041a875744f6554a814ff3a1385f
Evgeniy Ryazancev <ryazancev.e@gmail.com>
2020-12-04 14:38:06 +0300

    Начал 9е

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+const body = document.body;
+
+
+setInterval(function() {
+    const date = new Date();
+    const div = document.createElement('div');
+    div.textContent = date;
+    
+}, 1000)
 
 
 
+// function clock(){
+//     const date = new Date(),
+//         hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours(),
+//         minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes(),
+//         seconds = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
+//     console.log(hours + ':' + minutes + ':' + seconds);
+// }
+// setInterval(clock, 1000);
