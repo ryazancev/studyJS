0133736524e40db12e93f8690a897c6ee6e7a6f4
Evgeniy Ryazancev <ryazancev.e@gmail.com>
2020-12-09 22:23:30 +0300

    15 задание часть2

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 'use strict';
+
+class First {
+    hello() {
+        console.log('Привет, я метод родителя');
+    }
+};
+
+class Second extends First {
+    hello() {
+        super.hello();
+        console.log('А я наследуемый метод!');
+    }
+};
+
+const
+    obj1 = new First(),
+    obj2 = new Second();
+
+obj1.hello();
+obj2.hello();
