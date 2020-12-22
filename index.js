c2bab289700c938d4e9bdb3be36daa5a84e081b4
Evgeniy Ryazancev <ryazancev.e@gmail.com>
2020-12-22 12:38:36 +0300

    Задание 24

--- index.js
+++ index.js
@@ -264,19 +264,62 @@ window.addEventListener('DOMContentLoaded', () => {
 	changePhoto();
 
 	//Калькулятор
-	//Валидация
-	const calculateValidation = () => {
+	const calc = (price = 100) => {
 		const
 			calcBlock = document.querySelector('.calc-block'),
-			calcInputs = calcBlock.querySelectorAll('[type="text"]');
+			calcType = document.querySelector('.calc-type'),
+			calcSquare = document.querySelector('.calc-square'),
+			calcDay = document.querySelector('.calc-day'),
+			calcCount = document.querySelector('.calc-count'),
+			totalValue = document.getElementById('total');
 
-		calcInputs.forEach(element => {
-			element.addEventListener('input', () => {
-				element.value = element.value.replace(/\D/g, '');
+		//Валидация
+		const calculateValidation = () => {
+			const
+				calcInputs = calcBlock.querySelectorAll('[type="text"]');
+
+			calcInputs.forEach(element => {
+				element.addEventListener('input', () => {
+					element.value = element.value.replace(/\D/g, '');
+				});
 			});
+		};
+
+		calculateValidation();
+
+		const countSum = () => {
+			const
+				typeValue = calcType.options[calcType.selectedIndex].value,
+				squareValue = +calcSquare.value;
+			let total = 0,
+				countValue = 1,
+				dayValue = 1;
+
+			if (calcCount.value > 1) {
+				countValue += (calcCount.value - 1) / 10;
+			}
+
+			if (calcDay.value && calcDay.value < 5) {
+				dayValue *= 2;
+			} else if (calcDay.value && calcDay.value < 10) {
+				dayValue *= 1.5;
+			}
+
+			if (typeValue && squareValue) {
+				total = price * typeValue * squareValue * countValue * dayValue;
+			}
+
+			totalValue.textContent = total;
+		};
+
+		calcBlock.addEventListener('change', event => {
+			const target = event.target;
+
+			if (target.matches('select') || target.matches('input')) {
+				countSum();
+			}
 		});
 	};
 
-	calculateValidation();
-
+	calc(100);
 });
