608e4b64e24d5fb4d8fa776ab001d611c907db0f
Evgeniy Ryazancev <ryazancev.e@gmail.com>
2020-12-22 16:27:39 +0300

    18 усложненное

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 'use strict';
+const
+    input = document.querySelector('input'),
+    paragraph = document.querySelector('p');
+
+const debounce = (fn, msec) => {
+    let lastCall = 0,
+        lastCallTimer = 0;
+
+    return (...args) => {
+        const prevCall = lastCall;
+        lastCall = Date.now();
+        if (prevCall && (lastCall - prevCall) < msec) {
+            clearTimeout(lastCallTimer)
+        }
+        lastCallTimer = setTimeout(() => {
+            fn(...args)
+        }, msec)
+    }
+};
+
+const showText = value => {
+    paragraph.textContent = value;
+};
+
+const showTextDebounce = debounce(showText, 300)
+
+input.addEventListener('input', () => {
+    showTextDebounce(input.value)
+})
