d925fbaa14dada7c3da8cd665fb9a1b30dd796e5
Evgeniy Ryazancev <ryazancev.e@gmail.com>
2020-12-28 15:52:20 +0300

    lesson_28

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 'use strict';
+const output = document.querySelector('.output');
+
+const getData = (url, outputData) => {
+    const request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState !== 4) {
+            return
+        }
+        if (request.status === 200) {
+            const response = JSON.parse(request.responseText);
+            outputData(response); 
+        } else {
+            console.error(request.statusText);
+        }
+    });
+    request.send();
+};
+
+getData('https://jsonplaceholder.typicode.com/photos', data => {
+    console.log(data);
+});
