2cc178f896f55cbc3452b1a0d260398eb01dfbcd
Evgeniy Ryazancev <ryazancev.e@gmail.com>
2020-12-29 13:44:27 +0300

    lesson_28_hard

--- script.js
+++ script.js
@@ -5,24 +5,39 @@ document.addEventListener('DOMContentLoaded', () => {
         output = document.getElementById('output');
 
     select.addEventListener('change', () => {
-        const request = new XMLHttpRequest();
-        request.open('GET', './cars.json');
-        request.setRequestHeader('Content-type', 'application/json');
-        request.send();
-        request.addEventListener('readystatechange', () => {
-            if (request.readyState === 4 && request.status === 200) {
-                const data = JSON.parse(request.responseText);
-                data.cars.forEach(item => {
-                    if (item.brand === select.value) {
-                        const {brand, model, price} = item;
-                        output.innerHTML = `Тачка ${brand} ${model} <br>
-                        Цена: ${price}$`;
-                    }
-                });
-            } else {
-                output.innerHTML = 'Произошла ошибка';
-            }
-        });
-    });
+		let data;
 
+		const getData = () => {
+			return new Promise((resolve, reject) => {
+				const request = new XMLHttpRequest();
+				request.open('GET', './cars.json');
+				request.setRequestHeader('Content-type', 'application/json');
+				request.send();
+				request.addEventListener('readystatechange', () => {
+					if (request.readyState !== 4) return; 
+					if (request.status === 200) {
+						data = JSON.parse(request.responseText);
+						resolve();
+					} else {
+						reject(output.innerHTML = 'Произошла ошибка');
+					}
+				});
+			});
+		};
+
+		getData()
+			.then(() => {
+				data.cars.forEach(item => {
+					if (item.brand === select.value) {
+						const {brand, model, price} = item;
+						output.innerHTML = `Тачка ${brand} ${model} <br>
+						Цена: ${price}$`;
+					}
+				})
+			})
+			.catch(err => {
+				console.error(err);
+			})
+		
+    });
 });
