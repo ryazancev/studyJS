ede21685a38f5b1202f0f68d6eb6cc46c404898f
Evgeniy Ryazancev <ryazancev.e@gmail.com>
2020-12-30 21:45:17 +0300

    lesson_29_hard

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 'use strict';
-
-// https://api.exchangeratesapi.io/latest
+fetch('https://api.exchangeratesapi.io/latest')
+	.then(response => {
+		if (response.status !== 200) {
+			throw new Error('status neywork not 200')
+		}
+		return response.json();
+	})
+	.then(data => {
+		console.log(data); //евро
+	})
