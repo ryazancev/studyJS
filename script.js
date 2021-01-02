b8a15d7f1c8f11c34010646a5c8ea718557ed11b
Evgeniy Ryazancev <ryazancev.e@gmail.com>
2021-01-02 17:11:25 +0300

    lesson_29_hard

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 'use strict';
-fetch('https://api.exchangeratesapi.io/latest')
-	.then(response => {
-		if (response.status !== 200) {
-			throw new Error('status neywork not 200')
-		}
-		return response.json();
-	})
-	.then(data => {
-		console.log(data); //евро
-	})
-fetch('https://api.exchangeratesapi.io/latest?base=USD')
+
+const
+	urlEur = 'https://api.exchangeratesapi.io/latest',
+	urlUsd = 'https://api.exchangeratesapi.io/latest?base=USD';
+
+const getEur = url => {
+	fetch(url)
+		.then(response => {
+			if (response.status !== 200) {
+				throw new Error('status neywork not 200')
+			}
+			return response.json();
+		})
+		.then(data => {
+			rub = data.rates.RUB;
+			outputRub.value = Math.floor(+inputCurrency.value * rub);
+		})
+		.catch(err => console.warn(err));
+};
+
+const getUsd = url => {
+	fetch(url)
 	.then(response => {
 		if (response.status !== 200) {
 			throw new Error('status neywork not 200')
@@ -19,3 +30,8 @@ fetch('https://api.exchangeratesapi.io/latest?base=USD')
 	.then(data => {
 		console.log(data); //доллар
 	})
+};
+
+
+
+
