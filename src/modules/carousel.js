3508f1013af2626b8c83d8660d6002b0166b0563
Evgeniy Ryazancev <ryazancev.e@gmail.com>
2021-01-13 11:50:54 +0300

    Начал диплом

--- src/modules/carousel.js
+++ src/modules/carousel.js
@@ -2,7 +2,7 @@
 
 class Carousel {
 	constructor({ main, wrap, next, prev, infinity = false, slidesToShow = 3, responsive = [] }) {
-		if (!main || !wrap) {
+		if (!main || !wrap) { // Если мы не передали обертку слайдера и обертку слайдов
 			console.warn('slider-carousel: Необходимо 2 свойства, "main" и "wrap"!');
 		}
 
@@ -13,43 +13,43 @@ class Carousel {
 		this.prev = document.querySelector(prev);
 		this.slidesToShow = slidesToShow;
 		this.options = {
-			position: 0,
+			position: 0, // Начальная позиция слайда
 			infinity,
-			maxPosition: this.slides.length - this.slidesToShow,
-			widthSlide: Math.floor(100 / this.slidesToShow)
+			maxPosition: this.slides.length - this.slidesToShow, // Вычислим максимальную позицию
+			widthSlide: Math.floor(100 / this.slidesToShow) // Сколько слайдов будет на экране
 		};
 		this.responsive = responsive;
 	}
 
 	init() {
-		this.addClass();
-		this.addStyle();
+		this.addClass(); //Добавим классы на слайды
+		this.addStyle(); //Добавим стили на слайды
 
-		if (this.prev && this.next) {
-			this.controlSlider();
-		} else {
+		if (this.prev && this.next) {// Если у нас есть кнопки в разметке
+			this.controlSlider(); 
+		} else { // Если кнопок нет
 			this.addArrow();
 			this.controlSlider();
 		}
 
-		if (this.responsive) {
+		if (this.responsive) { //Если нужна адаптивность
 			this.responseInit();
 		}
 	}
 
 	addClass() {
-		this.main.classList.add('slider');
-		this.wrap.classList.add('slider__wrap');
+		this.main.classList.add('slider'); //Добавим класс для обертки слайдера
+		this.wrap.classList.add('slider__wrap'); //Добавим класс для обертки слайдов
 
 		for (const item of this.slides) {
-			item.classList.add('slider__item');
+			item.classList.add('slider__item'); // Добавим классы самим слайдам
 		}
 	}
 
 	addStyle() {
-		let style = document.getElementById('carousel-style');
+		let style = document.getElementById('carousel-style'); // Присвоим стили по id 
 
-		if (!style) {
+		if (!style) { // Если стилей с таким id нет, то добавим их
 			style = document.createElement('style');
 			style.id = 'carousel-style';
 		}
@@ -73,21 +73,21 @@ class Carousel {
 				margin: auto 0 !important;
 			}
 		`;
-		document.head.append(style);
+		document.head.append(style); // Добавим стили в разметку в конец тега head
 	}
 
-	controlSlider() {
-		this.prev.addEventListener('click', this.prevSlider.bind(this));
+	controlSlider() { // Повесим обработчик клик на кнопки
+		this.prev.addEventListener('click', this.prevSlider.bind(this)); 
 		this.next.addEventListener('click', this.nextSlider.bind(this));
 	}
 
 	prevSlider() {
-		if (this.options.infinity || this.options.position > 0) {
-			--this.options.position;
+		if (this.options.infinity || this.options.position > 0) { 
+			--this.options.position; // отнимаем начальную позицию по нажатию на <
 			if (this.options.position < 0) {
-				this.options.position = this.options.maxPosition;
+				this.options.position = this.options.maxPosition; // Если мы дошли до начала то вернемся к последнему слайду
 			}
-			this.wrap.style.transform = `translateX(-${this.options.position * this.options.widthSlide}%)`;
+			this.wrap.style.transform = `translateX(-${this.options.position * this.options.widthSlide}%)`; // Перемещаем слайды назад
 		}
 	}
 
@@ -95,13 +95,13 @@ class Carousel {
 		if (this.options.infinity || this.options.position < this.options.maxPosition) {
 			++this.options.position;
 			if (this.options.position > this.options.maxPosition) {
-				this.options.position = 0;
+				this.options.position = 0; // Если мы дошли до конца то вернемся к первому слайду
 			}
-			this.wrap.style.transform = `translateX(-${this.options.position * this.options.widthSlide}%)`;
+			this.wrap.style.transform = `translateX(-${this.options.position * this.options.widthSlide}%)`; //Перемещаем слайды вперед
 		}
 	}
 
-	addArrow() {
+	addArrow() { // Добавим кнопки если их нет в разметке
 		this.prev = document.createElement('button');
 		this.next = document.createElement('button');
 		this.prev.className = 'slider__prev';
@@ -139,13 +139,12 @@ class Carousel {
 	}
 
 	responseInit() {
-		const slidesToShowDefault = this.slidesToShow;
-		const allResponse = this.responsive.map(item => item.breackpoint);
-		const maxResponse = Math.max(...allResponse);
-		console.log(maxResponse);
+		const slidesToShowDefault = this.slidesToShow; // какое кол-во слайдов на экран по умолчанию
+		const allResponse = this.responsive.map(item => item.breackpoint); // Присвоим в переменную размеры дисплея
+		const maxResponse = Math.max(...allResponse); // Присвоим в переменную максимальный брейкпоинт
 
 		const checkResponse = () => {
-			const widthWindow = document.documentElement.clientWidth;
+			const widthWindow = document.documentElement.clientWidth; // Размеры окна без полосы прокрутки
 
 			if (widthWindow < maxResponse) {
 				for (let i = 0; i < allResponse.length; i++) {
