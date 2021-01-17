bd05b73868bdf6d4c9d50fa23c33e0652b3a6841
Evgeniy Ryazancev <ryazancev.e@gmail.com>
2021-01-17 14:59:10 +0300

    формы

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -23,30 +23,34 @@ const calc = () => {
 					return timeFraction;
 				},
 				draw(progress) {
-					if (priceMessage.value !== '' && priceMessage.value === 'ТЕЛО2019') {
-						if (cardLetoMozaika.checked) {
-							if (m1.checked) priceTotal.textContent = Math.ceil(progress * (1999 - 1999 / 100 * 30));
-							if (m2.checked) priceTotal.textContent = Math.ceil(progress * (9900 - 9900 / 100 * 30));
-							if (m3.checked) priceTotal.textContent = Math.ceil(progress * (13900 - 13900 / 100 * 30));
-							if (m4.checked) priceTotal.textContent = Math.ceil(progress * (19900 - 19900 / 100 * 30));
-						} else if (cardLetoSchelkovo.checked) {
-							if (m1.checked) priceTotal.textContent = Math.ceil(progress * (2999 - 2999 / 100 * 30));
-							if (m2.checked) priceTotal.textContent = Math.ceil(progress * (14990 - 14990 / 100 * 30));
-							if (m3.checked) priceTotal.textContent = Math.ceil(progress * (21990 - 21990 / 100 * 30));
-							if (m4.checked) priceTotal.textContent = Math.ceil(progress * (24990 - 24990 / 100 * 30));
+					try {
+						if (priceMessage.value !== '' && priceMessage.value === 'ТЕЛО2019') {
+							if (cardLetoMozaika.checked) {
+								if (m1.checked) priceTotal.textContent = Math.ceil(progress * (1999 - 1999 / 100 * 30));
+								if (m2.checked) priceTotal.textContent = Math.ceil(progress * (9900 - 9900 / 100 * 30));
+								if (m3.checked) priceTotal.textContent = Math.ceil(progress * (13900 - 13900 / 100 * 30));
+								if (m4.checked) priceTotal.textContent = Math.ceil(progress * (19900 - 19900 / 100 * 30));
+							} else if (cardLetoSchelkovo.checked) {
+								if (m1.checked) priceTotal.textContent = Math.ceil(progress * (2999 - 2999 / 100 * 30));
+								if (m2.checked) priceTotal.textContent = Math.ceil(progress * (14990 - 14990 / 100 * 30));
+								if (m3.checked) priceTotal.textContent = Math.ceil(progress * (21990 - 21990 / 100 * 30));
+								if (m4.checked) priceTotal.textContent = Math.ceil(progress * (24990 - 24990 / 100 * 30));
+							}
+						} else {
+							if (cardLetoMozaika.checked) {
+								if (m1.checked) priceTotal.textContent = Math.ceil(progress * 1999);
+								if (m2.checked) priceTotal.textContent = Math.ceil(progress * 9900);
+								if (m3.checked) priceTotal.textContent = Math.ceil(progress * 13900);
+								if (m4.checked) priceTotal.textContent = Math.ceil(progress * 19900);
+							} else if (cardLetoSchelkovo.checked) {
+								if (m1.checked) priceTotal.textContent = Math.ceil(progress * 2999);
+								if (m2.checked) priceTotal.textContent = Math.ceil(progress * 14990);
+								if (m3.checked) priceTotal.textContent = Math.ceil(progress * 21990);
+								if (m4.checked) priceTotal.textContent = Math.ceil(progress * 24990);
+							}
 						}
-					} else {
-						if (cardLetoMozaika.checked) {
-							if (m1.checked) priceTotal.textContent = Math.ceil(progress * 1999);
-							if (m2.checked) priceTotal.textContent = Math.ceil(progress * 9900);
-							if (m3.checked) priceTotal.textContent = Math.ceil(progress * 13900);
-							if (m4.checked) priceTotal.textContent = Math.ceil(progress * 19900);
-						} else if (cardLetoSchelkovo.checked) {
-							if (m1.checked) priceTotal.textContent = Math.ceil(progress * 2999);
-							if (m2.checked) priceTotal.textContent = Math.ceil(progress * 14990);
-							if (m3.checked) priceTotal.textContent = Math.ceil(progress * 21990);
-							if (m4.checked) priceTotal.textContent = Math.ceil(progress * 24990);
-						}
+					} catch (error) {
+						console.warn(error);
 					}
 				}
 			});
--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -3,15 +3,82 @@ import maskPhone from './maskPhone';
 
 const sendForm = () => {
 	// Получим все формы со страницы
-	const forms = document.querySelectorAll('form');
+	const forms = document.querySelectorAll('form'),
+		errorMessage = 'Что-то пошло не так...',
+		successMessage = 'Заявка отправлена',
+		statusMessage = document.createElement('div'),
+		popup = document.getElementById('thanks'),
+		textMessage = popup.querySelector('p'),
+		radioLetoMozaika = document.getElementById('footer_leto_mozaika'),
+		radioLetoSchelkovo = document.getElementById('footer_leto_schelkovo');
+
+	const postData = body => fetch('../../server.php', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(body)
+	});
 
 	forms.forEach(form => {
-		form.addEventListener('change', event => {
+		// Повесим на инпуты, маску и валидацию
+		form.addEventListener('input', () => {
+			// Выберем только те инпуты, которые нас интересуют
+			for (const elem of form.elements) {
+				if (elem.type === 'text') elem.value = elem.value.replace(/[^А-яа-я ]/, '');
+				if (elem.type === 'tel') maskPhone('[type="tel"]');
+			}
+		});
+
+		form.addEventListener('submit', event => {
 			event.preventDefault();
+			//Валидация
+			statusMessage.style.color = '#f74104';
+			statusMessage.style.marginTop = '5px';
+			form.append(statusMessage);
+
+			if (form.id !== 'footer_form') {
+				for (const elem of form.elements) {
+					if (elem.type === 'checkbox' && !elem.checked) {
+						statusMessage.textContent = 'Вы должны дать согласие на обработку персональных данных';
+						return;
+					} else {
+						if (elem.type === 'text') {
+							if (elem.value.length < 2 || elem.value.length > 50) {
+								elem.value = '';
+								statusMessage.textContent = 'Имя должно включать не менее 2 и не более 50 символов';
+								setTimeout(() => {
+									statusMessage.textContent = '';
+								}, 3000);
+								return;
+							}
+						}
+					}
+				}
+			} else {
+				for (const elem of form.elements) {
+					if (!radioLetoMozaika.checked && !radioLetoSchelkovo.checked) {
+
+						console.dir(radioLetoMozaika);
+						console.dir(radioLetoSchelkovo);
+						statusMessage.textContent = 'Вы должны выбрать клуб';
+						setTimeout(() => {
+							statusMessage.textContent = '';
+						}, 3000);
+						return;
+					}
+				}
+			}
+
+			
+			// Откроем модальное окно
+			popup.style.display = 'block';
+			// Добавим в модальное окно наше сообщение для пользователя
+			// textMessage.innerHTML
 		});
 	});
 
-	
+
 };
 
 export default sendForm;
